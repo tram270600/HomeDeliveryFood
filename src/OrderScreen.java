ff3cb84d0d1c389a3b3585385d4102eeddf4aa9d
nguyen123happy1 <63740529+nguyen123happy1@users.noreply.github.com>
2020-05-12 15:47:51 +0700

    Update OrderScreen.java

--- src/OrderScreen.java
+++ src/OrderScreen.java
@@ -13,7 +13,7 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class FoodTable {
+public class OrderScreen {
 
 	private JFrame frame;
 	private JTable table;
