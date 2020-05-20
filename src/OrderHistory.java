6aa2f7982bb0baf021018e6f046880b8e68f41a5
nguyen123happy1 <63740529+nguyen123happy1@users.noreply.github.com>
2020-05-20 23:48:01 +0700

    Update OrderHistory.java

--- src/OrderHistory.java
+++ src/OrderHistory.java
@@ -15,7 +15,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class OrderHistory {
-
+	private JDialog jDialog1;
 	private JFrame frmOrderHistory;
 	private JTable jTable_display_order;
 	private Customer customer;
@@ -58,7 +58,7 @@ public class OrderHistory {
 			public void run() {
 				try {
 					OrderHistory window = new OrderHistory();
-					window.frmOrderHistory.setVisible(true);
+					window.jDialog1.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -78,16 +78,56 @@ public class OrderHistory {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frmOrderHistory = new JFrame();
-		frmOrderHistory.setTitle("Order History");
-		frmOrderHistory.setBounds(375, 170, 657, 366);
-		frmOrderHistory.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		frmOrderHistory.getContentPane().setLayout(null);
+// 		frmOrderHistory = new JFrame();
+// 		frmOrderHistory.setTitle("Order History");
+// 		frmOrderHistory.setBounds(375, 170, 657, 366);
+// 		frmOrderHistory.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+// 		frmOrderHistory.getContentPane().setLayout(null);
 
-		JScrollPane scrollPane = new JScrollPane();
+// 		JScrollPane scrollPane = new JScrollPane();
+// 		scrollPane.setBounds(10, 11, 621, 168);
+// 		frmOrderHistory.getContentPane().add(scrollPane);
+
+// 		jTable_display_order = new JTable();
+// 		jTable_display_order.setModel(new DefaultTableModel(
+// 			new Object[][] {
+// 			},
+// 			new String[] {
+// 				"ServerID", "OrderID", "ResNo", "CusNo", "ShipNo", "Order Price", "Order Status"
+// 			}
+// 		) {
+// 			Class[] columnTypes = new Class[] {
+// 				String.class, Integer.class, String.class, Integer.class, Integer.class, String.class, String.class
+// 			};
+// 			public Class getColumnClass(int columnIndex) {
+// 				return columnTypes[columnIndex];
+// 			}
+// 			boolean[] columnEditables = new boolean[] {
+// 				false, true, true, true, true, true, true
+// 			};
+// 			public boolean isCellEditable(int row, int column) {
+// 				return columnEditables[column];
+// 			}
+// 		});
+// 		scrollPane.setViewportView(jTable_display_order);
+		
+// 		btnNewButton = new JButton("Back");
+// 		btnNewButton.addActionListener(new ActionListener() {
+// 			public void actionPerformed(ActionEvent e) {
+// 				Customer.activity();
+// 				frmOrderHistory.setVisible(false);
+// 			}
+// 		});
+// 		btnNewButton.setBounds(275, 231, 89, 23);
+// 		frmOrderHistory.getContentPane().add(btnNewButton);
+// 		//scrollPane.getViewport().add(jTable_display_order, null);
+		jDialog1 = new javax.swing.JDialog();
+        jDialog1.setModal(true);
+        jDialog1.setLocation(400, 200);
+       
+        JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(10, 11, 621, 168);
-		frmOrderHistory.getContentPane().add(scrollPane);
-
+        
 		jTable_display_order = new JTable();
 		jTable_display_order.setModel(new DefaultTableModel(
 			new Object[][] {
@@ -102,25 +142,39 @@ public class OrderHistory {
 			public Class getColumnClass(int columnIndex) {
 				return columnTypes[columnIndex];
 			}
-			boolean[] columnEditables = new boolean[] {
-				false, true, true, true, true, true, true
-			};
-			public boolean isCellEditable(int row, int column) {
-				return columnEditables[column];
-			}
 		});
 		scrollPane.setViewportView(jTable_display_order);
 		
 		btnNewButton = new JButton("Back");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				jDialog1.setVisible(false);
 				Customer.activity();
-				frmOrderHistory.setVisible(false);
 			}
 		});
-		btnNewButton.setBounds(275, 231, 89, 23);
-		frmOrderHistory.getContentPane().add(btnNewButton);
-		//scrollPane.getViewport().add(jTable_display_order, null);
 		
+		javax.swing.GroupLayout jDialog1Layout = new javax.swing.GroupLayout(jDialog1.getContentPane());
+		jDialog1Layout.setHorizontalGroup(
+			jDialog1Layout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(jDialog1Layout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 600, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(10, Short.MAX_VALUE))
+				.addGroup(jDialog1Layout.createSequentialGroup()
+					.addContainerGap(555, Short.MAX_VALUE)
+					.addComponent(btnNewButton)
+					.addContainerGap())
+		);
+		jDialog1Layout.setVerticalGroup(
+			jDialog1Layout.createParallelGroup(Alignment.LEADING)
+				.addGroup(jDialog1Layout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 200, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED, 19, Short.MAX_VALUE)
+					.addComponent(btnNewButton)
+					.addContainerGap())
+		);
+        jDialog1.getContentPane().setLayout(jDialog1Layout);
+        jDialog1.pack();
 	}
 }
